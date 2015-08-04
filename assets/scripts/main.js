70aa89294fe4a4c3a61097629efcb27d1859c6d7
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-04 13:58:36 -0400

    Fixes Handlebars listing functions as modules in view.js.
    ui.js becomes click handler, calling on View.method, and
    main.js is cleared for now.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,11 +2,7 @@
 
 //data for testing
 
-var data = [
-  {occurs_on: "2015-08-11", title:"RLLY COOL EVENT GUYS", venue:"some dev shop", description:"you'll like it tho", link:"heresalink", number_going:5},
-  {occurs_on: "2015-08-12", title:"SRLSY COOL EVENT", venue:"some dev shop", description:"srsly tho the best event", link:"heresalink", number_going:7},
-  {occurs_on: "2015-08-13", title:"LOTSA FREE SHIT", venue:"better dev shop", description:"like pizza n beer n tshirts n shit", link:"heresalink", number_going:17}
-];
+
 
 
 
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,14 +2,7 @@
 
 $(document).ready(function(){
 
-  //handlebars templating function
-  var eventIndexTemplateFunction = Handlebars.compile($("#event-index").html());
-
-  //call templating function with object events as parameter
-  var newHTML = eventIndexTemplateFunction({events: data});
-
-  //set element event-index's HTML to newHTML
-  $("#event-index").html(newHTML);
+  $("#show-events").on('click', View.showEvents);
 
 });
 
