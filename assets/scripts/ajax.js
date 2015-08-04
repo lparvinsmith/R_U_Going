f07b2bef6653b6e3ea078b6cb818bbd6d13ca5c1
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-03 23:15:39 -0400

    Adds boilerplate Ajax for register, login, and create event.

--- assets/scripts/ajax.js
+++ assets/scripts/ajax.js
@@ -1,14 +1,72 @@
-'use strict';
+//wrapping in jQ function necessary?
+$(function(){
+  'use strict';
 
-//when document.ready, show all events in chronological order
-// $(document).ready(){};
+var sa = '//localhost:3000';
 
 //when click button 'register', register user
+//will this automatically add user to users table?
+  $('#register').on('click', function(e){
+    $.ajax(sa + '/register', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $('#email').val(),
+          password: $('#password').val(),
+          // password_confirmation: $('#password').val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+      //see api.jquery documentation online for meanings of all of these
+    }).done(function(data,textStatus,jqxhr){
+      console.log(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      console.log('registration failed');
+    })
+  });
 
 //when click button 'login', login user
+  $('#login').on('click', function(e){
+    $.ajax(sa + '/login', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+        credentials: {
+          email: $("#email").val(),
+          password: $("#password").val()
+        }
+      }),
+      dataType: 'json',
+      method: 'POST'
+    }).done(function(data){
+      console.log(data.token);
+    }).fail(function(e){
+      console.log('login failed');
+    });
+  });
 
-//when click button 'add-event', create new event
-//user who creates event is recorded as "going"
+//when click button 'create-event', create new event
+//user who creates event is recorded as "going"?
+  $('#create').on('click', function(e) {
+    $.ajax(sa + '/events', {
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({}),
+      dataType: 'json',
+      method: 'POST',
+      headers: {
+        Authorization: 'Token token=' + $('#token').val()
+      }
+    }).done(function(data, textStatus, jqxhr){
+      console.log(JSON.stringify(data));
+    }).fail(function(jqxhr, textStatus, errorThrown){
+      console.log('create failed');
+    });
+  });
+
+//THESE BUTTONS DON'T EXIST YET
 
 //when click button 'update-event', update new event
 
@@ -17,3 +75,4 @@
 //when click button 'im-going!', add to user's id to event database
 
 
+});
