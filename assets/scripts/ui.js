7bf6176f254a7a12da92bde2300b3604bf1875b3
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-03 22:12:45 -0400

    Writes Handlebars functions and provides fake events, but still not working.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,7 +1,15 @@
 'use strict';
 
-//when click button 'i'm going!', do something
-$("#im-going").on('click',function(event){
-  console.log("good for you!");
+$(document).ready(function(){
+
+  //handlebars templating function
+  var eventIndexTemplateFunction = Handlebars.compile($("#event-index").html());
+
+  //call templating function with object events as parameter
+  var newHTML = eventIndexTemplateFunction({events: data});
+
+  //set element event-index's HTML to newHTML
+  $("#event-index").html(newHTML);
+
 });
 
