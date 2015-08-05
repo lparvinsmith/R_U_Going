4c70deeda91a21dc56b4c4622fd481e0ba98e0e2
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-05 15:47:06 -0400

    When click button 'update' on each event, shows update modal.
    Fields for date-time and title are populated, now have to do others.
    Puts click handler for update button in ui.js.

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,11 +2,16 @@
 
 $(document).ready(function(){
 
-  // View.showEvents(); //Why doesn't this work?
-
-  // $("#show-events").on('click', View.showEvents);
-
-  // $("#register").on('click', MyApi.register);
+  $('#eventUpdateModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var title = button.data('title');
+    var occurs = button.data('occurs'); // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('#event-date-time').val(occurs);
+    modal.find('#event-title').val(title);
+  });
 
 });
 
